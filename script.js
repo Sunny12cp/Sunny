23dec05bece4a957b65bb802a17e647d223d5c8c
Sunny12cp <114519415+Sunny12cp@users.noreply.github.com>
2023-12-02 09:36:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,59 @@
-function updateTextOverlay() {
-    const textOverlay = document.getElementById('textOverlay').value;
-    const socket = new WebSocket('ws://your_server_url');
+// script.js
+let localStream;
+let peer;
 
-    socket.addEventListener('open', function (event) {
-        socket.send(JSON.stringify({ type: 'textOverlay', data: textOverlay }));
-    });
+function startPreview() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        .then(stream => {
+            localStream = stream;
+            document.getElementById('localVideo').srcObject = stream;
+
+            // Create a PeerJS connection
+            const peer = new Peer();
+
+            // Listen for incoming connections
+            peer.on('open', id => {
+                console.log('My peer ID is: ' + id);
+            });
+
+            peer.on('connection', conn => {
+                console.log('Peer connected:', conn.peer);
+
+                // Handle incoming data, signaling, etc.
+                handleConnection(conn);
+            });
+
+            // Send our stream to the remote peer
+            const conn = peer.connect('some_other_peer_id', { reliable: true });
+            conn.on('open', () => {
+                console.log('Connection open to peer:', conn.peer);
+                conn.send({ type: 'stream', stream: localStream });
+            });
+        })
+        .catch(error => {
+            console.error('Error accessing camera and/or microphone:', error);
+        });
 }
 
-const socket = new WebSocket('ws://your_server_url');
-
-socket.addEventListener('message', function (event) {
-    const data = JSON.parse(event.data);
-
-    if (data.type === 'textOverlayUpdate') {
-        const updatedTextOverlay = data.data;
-        console.log('Received text overlay update:', updatedTextOverlay);
-    }
-});
-
 function startStreaming() {
-    const streamUrl = document.getElementById('streamUrl').value;
-    const streamKey = document.getElementById('streamKey').value;
-    const fileInput = document.getElementById('fileInput');
-    const screenSize = document.getElementById('screenSize').value;
-    const textOverlay = document.getElementById('textOverlay').value;
-    const textSize = document.getElementById('textSize').value;
-    const textFont = document.getElementById('textFont').value;
-    const textColor = document.getElementById('textColor').value;
-    const textAlign = document.getElementById('textAlign').value;
+    // Implement logic to start the actual live streaming
+    // This might involve signaling to another peer or server
+    // and establishing a connection for live video streaming
+    console.log('Implement logic to start live streaming');
+}
 
-    if (streamUrl && streamKey) {
-        if (fileInput.files.length > 0) {
-            const file = fileInput.files[0];
+function handleConnection(conn) {
+    conn.on('data', data => {
+        // Handle incoming data from the remote peer
+        console.log('Received data from peer:', data);
 
-            const formData = new FormData();
-            formData.append('file', file);
-            formData.append('screenSize', screenSize);
-            formData.append('textOverlay', textOverlay);
-            formData.append('textSize', textSize);
-            formData.append('textFont', textFont);
-            formData.append('textColor', textColor);
-            formData.append('textAlign', textAlign);
-
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', 'upload.php', true);
-            xhr.onload = function () {
-                if (xhr.status === 200) {
-                    console.log('File uploaded successfully:', file.name);
-                    console.log('Screen Size selected:', screenSize);
-                    console.log('Text Overlay:', textOverlay);
-                    console.log('Text Size:', textSize);
-                    console.log('Text Font:', textFont);
-                    console.log('Text Color:', textColor);
-                    console.log('Text Alignment:', textAlign);
-
-                    alert("Streaming started! Redirect to your streaming page or initiate streaming process.");
-                } else {
-                    alert('File upload failed.');
-                }
-            };
-            xhr.send(formData);
-        } else {
-            alert("No file selected. Please upload or select a file.");
+        if (data.type === 'stream') {
+            // Display remote stream on the 'remoteVideo' element
+            document.getElementById('remoteVideo').srcObject = data.stream;
         }
-    } else {
-        alert("Please enter both stream URL and stream key.");
-    }
+    });
+
+    conn.on('close', () => {
+        console.log('Connection closed:', conn.peer);
+    });
 }
